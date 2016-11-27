2ccf3fde004ce1c91ecabbc49ec0ff28d45b0b05
jonfee <337883612@qq.com>
2016-11-27 21:27:38 +0800

    原型模式

---
+++ DesignPatternExample/Prototype/Resume.cs
@@ -0,0 +1,50 @@
+﻿namespace DesignPatternExample.Prototype
+{
+    public class Resume
+    {
+        private string name;
+        private string sex;
+        private int age;
+        private WorkExperience work;
+
+        public Resume() { }
+
+        private Resume(WorkExperience work)
+        {
+            this.work = (WorkExperience)work.Clone();
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public string Sex
+        {
+            get { return this.sex; }
+            set { this.sex = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public WorkExperience Work
+        {
+            get { return this.work; }
+            set { this.work = value; }
+        }
+
+        public object Clone()
+        {
+            Resume obj = new Resume(this.work);
+            obj.Name = name;
+            obj.Sex = sex;
+            obj.Age = age;
+            return obj;
+        }
+    }
+}
---
+++ DesignPatternExample/Prototype/WorkExperience.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPatternExample.Prototype
+{
+    public class WorkExperience
+    {
+        private string workDate;
+        public string WorkDate
+        {
+            get { return this.workDate; }
+            set { this.workDate = value; }
+        }
+
+        private string company;
+        public string Company
+        {
+            get { return this.company; }
+            set { this.company = value; }
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
